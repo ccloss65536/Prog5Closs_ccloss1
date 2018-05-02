eccfe469695423556dd370804c8bd97e318dcb67
Carl Closs <ccloss1@cs.binghamton.edu>
2018-05-02 18:08:48 -0400

    Added consumer interface to producer-consumer setup

--- disk_sched.c
+++ disk_sched.c
@@ -2,6 +2,14 @@
 #include <pthread.h>
 #include "common.h"
 
-
+void take_request(){
+	pthread_mutex_lock(&request_condition_mutex);
+	while(num_requests <= 0) pthread_cond_wait(&request_fill, &request_condition_mutex);
+	//Peter's code here
+	next_to_do = (next_to_do + 1) % max_requests;
+	num_requests--; 
+	pthread_cond_signal(&request_fill);
+	pthread_mutex_unlock(&request_condition_mutex);
+}
 
 
